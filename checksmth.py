6daa1a17b37b7af9fb97df5e685b358f946c8ec9
scateu <scateu@laser>
2012-10-03 00:15:38 +0800

    加入了倒计时

--- checksmth.py
+++ checksmth.py
@@ -1,14 +1,33 @@
 #-*- coding:utf8
 import feedparser
-BoardName = 'SecondDigi'
-KeyWords = [u'iPhone',u'黑莓',u'Blackberry']
+import time
+BoardName = 'HouseRent'
+KeyWords = [u'清华']
 
-def check(BoardName,KeyWords,interval=60,mailto='scateu@gmail.com'):
+def check(BoardName,KeyWords):
+    result = []
     URL = 'http://www.newsmth.net/nForum/rss/board-'+BoardName
     feed = feedparser.parse(URL)
     for i in feed.entries:
         for k in KeyWords:
             if (i.title.lower().find(k.lower()) != -1) or (i.description.lower().find(k.lower()) != -1):
+                desc = i.description.replace('<br />','\n').replace('&nbsp;',' ')
                 print i.title
-                print i.description.replace('<br />','\n').replace('&nbsp;',' ')
+                print desc
                 print i.link
+                result.append('\n'.join([desc,i.link]))
+    return result
+
+def countdown(sec=10):
+    print u'Sleep...'
+    while sec >= 0:
+        print sec
+        time.sleep(1)
+        sec -= 1
+
+while True:
+    if check(BoardName,KeyWords):
+        print u'找到了~'
+    else:
+        print u'啥都没...'
+    countdown(10)

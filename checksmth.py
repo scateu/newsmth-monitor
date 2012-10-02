02a3a52f6bea1a9a3e0078781ee716905b738088
scateu <scateu@laser>
2012-10-03 00:23:57 +0800

    增加了重复帖子不理睬机制

--- checksmth.py
+++ checksmth.py
@@ -1,10 +1,11 @@
 #-*- coding:utf8
 import feedparser
 import time
-BoardName = 'HouseRent'
-KeyWords = [u'清华']
 
+
+CheckedLinks = []
 def check(BoardName,KeyWords):
+    global CheckedLinks
     result = []
     URL = 'http://www.newsmth.net/nForum/rss/board-'+BoardName
     feed = feedparser.parse(URL)
@@ -12,22 +13,31 @@ def check(BoardName,KeyWords):
         for k in KeyWords:
             if (i.title.lower().find(k.lower()) != -1) or (i.description.lower().find(k.lower()) != -1):
                 desc = i.description.replace('<br />','\n').replace('&nbsp;',' ')
-                print i.title
-                print desc
-                print i.link
-                result.append('\n'.join([desc,i.link]))
+#                print i.title
+#                print desc
+#                print i.link
+                if i.link not in CheckedLinks:
+                    result.append('\n'.join([desc,i.link]))
+                    CheckedLinks.append(i.link)
     return result
 
 def countdown(sec=10):
-    print u'Sleep...'
+    print u'Sleep...',
     while sec >= 0:
-        print sec
+        print sec,
         time.sleep(1)
         sec -= 1
+    print
 
-while True:
-    if check(BoardName,KeyWords):
-        print u'找到了~'
-    else:
-        print u'啥都没...'
-    countdown(10)
+def main():
+    BoardName = 'HouseRent'
+    KeyWords = [u'清华']
+    while True:
+        if check(BoardName,KeyWords):
+            print u'找到了~'
+        else:
+            print u'啥都没...'
+        countdown(10)
+
+if __name__ == "__main__":
+    main()

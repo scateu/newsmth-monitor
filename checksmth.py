9c1db3db4a8a8fd8b766828b767d96647c501745
scateu <scateu@laser>
2012-10-03 15:02:44 +0800

    增加了OptParser 可以通过命令行来指定 版面，关键字，邮件地址，时间间隔

--- checksmth.py
+++ checksmth.py
@@ -2,6 +2,8 @@
 import feedparser
 import time
 import ramlab_mail
+from optparse import OptionParser
+
 
 
 CheckedLinks = []
@@ -31,8 +33,26 @@ def countdown(sec=10):
     print
 
 def main():
-    BoardName = 'HouseRent'
-    KeyWords = [u'清华',u'南门',u'西北小区',u'西北社区',u'五道口',u'校内',u'照澜院']
+    parser = OptionParser()
+    parser.add_option("-b","--board",dest="BoardName",help="The BoardName to be watched")
+    parser.add_option("-k","--keywords",dest="KeyWords",type="string",help="Words to be watched")
+    parser.add_option("-m","--mail-to",dest="MailTo",help="Notify e-mail address when found")
+    parser.add_option("-i","--interval",dest="Interval",type="int",help="Check interval, default = 10 sec")
+
+    (options,args) = parser.parse_args()
+
+    if not options.BoardName or not options.KeyWords:
+        print "args not given"
+
+    try:
+        BoardName = options.BoardName
+        KeyWords =  [i.decode('utf8') for i in options.KeyWords.split(',')]
+    except:
+        print "Args Error."
+        return
+
+        #BoardName = 'HouseRent'
+        #KeyWords = [u'清华',u'南门',u'西北小区',u'西北社区',u'五道口',u'校内',u'照澜院']
     m = ramlab_mail.ramlab_mail()
 
     while True:
@@ -42,12 +62,18 @@ def main():
         result = check(BoardName,KeyWords)
         if result:
             print u'找到了~'
-            msg = u'\n'.join(result)
+            msg = u'\n\n\n\n\n\n\n*********苦逼分隔线*************\n\n'.join(result)
             print msg
-            m.sendmail(to='scateu@gmail.com',subject=monitor_string,message=msg)
+            if options.MailTo:
+                m.sendmail(to=options.MailTo,subject=monitor_string,message=msg)
+            else:
+                print "No Email Address"
         else:
             print u'没啥新的...'
-        countdown(10)
+        if options.Interval:
+            countdown(options.Interval)
+        else:
+            countdown(10)
 
 if __name__ == "__main__":
     main()

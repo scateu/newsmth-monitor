fedebfbbc1796054d2d666a574f24fd264816200
scateu <scateu@laser>
2013-01-13 06:08:49 +0800

    add public license

--- checksmth.py
+++ checksmth.py
@@ -1,4 +1,20 @@
 #-*- coding:utf8
+"""
+ DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                    Version 2, December 2004
+
+ Copyright (C) 2012 Romain Lespinasse <romain.lespinasse@gmail.com>
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+"""
 import feedparser
 import time
 import mysmtp
--- mysmtp.py
+++ mysmtp.py
@@ -1,11 +1,27 @@
 # -*- coding:utf8
+"""
+ DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                    Version 2, December 2004
+
+ Copyright (C) 2012 Romain Lespinasse <romain.lespinasse@gmail.com>
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+"""
 import smtplib
 from email.mime.text import MIMEText
 from email.Header import Header
 
 class mymail():
     """
-    Library to sendmail from ramlab_things@126.com
+    Library to sendmail 
     see sendmail()
     """
     def __init__(self):
